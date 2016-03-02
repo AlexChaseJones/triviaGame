d2bdee5008557a490a8255337ffb43c51aab3c1f
Alexander <alexchasejones@icloud.com>
2016-03-02 15:24:07 -0500

    begun work and javascript objects

--- javascript/app.js
+++ javascript/app.js
@@ -1,8 +1,48 @@
 var pageBuilder = {
 	$wrapper : $('#wrapper'),
 	prepareView : function() {
-		var newDiv = $('<div id="prepareContainer"><h2>title</h2></div>');
+		var $newDiv = $('<div id="prepareContainer"><h1>Prepare for the Next Question!</h1></div>');
 		(this.$wrapper).empty();
-		(this.$wrapper).append(newDiv)
-	}	
+		(this.$wrapper).append($newDiv);
+	},
+	questionView : function(){
+		var $newDiv = $('<div id="questionContainer">');
+
+
+		for (var i = 0; i < 4; i++) {
+			var $possibleAnswer = $('<div class="choice">');
+			($possibleAnswer).html('<h2>' + allQuestions[userStats.questionNumber].answers[i] + '</h2>');
+			($newDiv).append($possibleAnswer);
+		}
+
+		(this.$wrapper).empty();
+		(this.$wrapper).append($newDiv);
+	},
 };
+
+var allQuestions = [
+	one = {
+		question: "Where was Bill Clinton Born?",
+		answers : ['arkansas', 'mississsippi', 'alabama', 'lousianna'],
+		correct : function() {
+			return this.answers[1];
+		}
+	},
+	two = {
+
+	},
+	three = {
+
+	},
+];
+
+var userStats = {
+	questionNumber : 0,
+};
+
+
+
+
+
+
+
