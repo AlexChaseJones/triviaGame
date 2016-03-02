a548851a7df688ba3e803720eb701cd98526f46b
Alexander <alexchasejones@icloud.com>
2016-03-02 15:43:14 -0500

    added afterQuestionView function in pageBuilder object

--- javascript/app.js
+++ javascript/app.js
@@ -1,31 +1,40 @@
 var pageBuilder = {
 	$wrapper : $('#wrapper'),
 	prepareView : function() {
-		var $newDiv = $('<div id="prepareContainer"><h1>Prepare for the Next Question!</h1></div>');
+		var $prepare = $('<div id="prepareContainer"><h1>Prepare for the Next Question!</h1></div>');
 		(this.$wrapper).empty();
-		(this.$wrapper).append($newDiv);
+		(this.$wrapper).append($prepare);
 	},
 	questionView : function(){
-		var $newDiv = $('<div id="questionContainer">');
+		var $question = $('<div id="questionContainer">');
+		($question).html('<h1>' + allQuestions[userStats.questionNumber].question + '</h1>');
 
-
+		var $choices = $('<div id="choicesContainer">')
 		for (var i = 0; i < 4; i++) {
 			var $possibleAnswer = $('<div class="choice">');
 			($possibleAnswer).html('<h2>' + allQuestions[userStats.questionNumber].answers[i] + '</h2>');
-			($newDiv).append($possibleAnswer);
-		}
+			($choices).append($possibleAnswer);
+		};
 
 		(this.$wrapper).empty();
-		(this.$wrapper).append($newDiv);
+		(this.$wrapper).append($question);
+		(this.$wrapper).append($choices);
+	},
+	afterQuestionView : function() {
+		var $answer = $('<div class="answerContainer">');
+		($answer).html('<h1>The answer was ' + allQuestions[userStats.questionNumber].correct() + '</h1>');
+		(this.$wrapper).empty();
+		(this.$wrapper).append($answer);
+
 	},
 };
 
 var allQuestions = [
 	one = {
 		question: "Where was Bill Clinton Born?",
-		answers : ['arkansas', 'mississsippi', 'alabama', 'lousianna'],
+		answers : ['Arkansas', 'Mississsippi', 'Alabama', 'Lousianna'],
 		correct : function() {
-			return this.answers[1];
+			return this.answers[0];
 		}
 	},
 	two = {
@@ -38,6 +47,8 @@ var allQuestions = [
 
 var userStats = {
 	questionNumber : 0,
+	choice : 'Arkansas',
+
 };
 
 
